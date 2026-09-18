fab1f0441ba71cf388827d656d7559891a381943
tonykastaneda <tonykastaneda@gmail.com>
2026-09-17 22:03:21 -0700

    Update yakkity to 0.3.1

--- Formula/yakkity.rb
+++ Formula/yakkity.rb
@@ -1,8 +1,8 @@
 class Yakkity < Formula
   desc "Search, resume, and hand off conversations between coding agents"
   homepage "https://yakkity.dev"
-  url "https://github.com/tonykastaneda/yakkity/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "0b08c2b1fa5be2a99282d0345807eb4dde7dc1891fc2a5b414e79b829a38c6cc"
+  url "https://github.com/tonykastaneda/yakkity/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "df8c7f01a9e64970039d1c2febceed9e220353432434c0d4cd377d1c5cdfc8cc"
   license "MIT"
   head "https://github.com/tonykastaneda/yakkity.git", branch: "main"
 
@@ -16,7 +16,7 @@ class Yakkity < Formula
   end
 
   test do
-    assert_match "yakkity 0.3.0", shell_output("#{bin}/yakk --version")
+    assert_match "yakkity 0.3.1", shell_output("#{bin}/yakk --version")
     assert_match "Usage: yakk", shell_output("#{bin}/yakk --help")
   end
 end
